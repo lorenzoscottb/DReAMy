8ab52b204343023521b72deec5f5df003c0a3cc9
Lorenzo Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2023-02-09 21:34:56 +0000

    Add files via upload

--- dreamy/emotion_classification/models_names.py
+++ dreamy/emotion_classification/models_names.py
@@ -9,23 +9,23 @@ bert_base_emotion_class   = "DReAMy-lib/bert-base-cased-DreamBank-emotion-presen
 t5_genration_char         = "DReAMy-lib/t5-base-DreamBank-Generation-Emot-Char"
 
 
-models_dict = {
+emotion_models_dict = {
 	"Custom architecture, best for multi-labell classification": best_paper_model,
 	"Large Multilingual model (XLM-R)": xlm_roberta_emotion_class,
 	" (base) English-Only model (BERT-base)": bert_base_emotion_class,
 	"Text-Generation (T5), Characters + numberd emotions": t5_genration_char
 }
 
-def get_names():
-	print(models_dict)
+def get_emotion_names():
+	print(emotion_models_dict)
 
 
-model_maps = {
+emotion_model_maps = {
 	"presence-large-en": [best_paper_model, "none"],
 	"presence-base-en": [bert_base_emotion_class, "text-classification"],
 	"presence-large-multi": [xlm_roberta_emotion_class, "text-classification"],
 	"generation-base-en": [t5_genration_char, "summarization"],
 }
 
-def get_maps():
-	print(model_maps)
+def get_emotion_maps():
+	print(emotion_model_maps)
