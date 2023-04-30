2bd77fe0e6bc2577a3d5b4188ad195b6cd8a662d
Lorenzo Bertolini <39827031+lorenzoscottb@users.noreply.github.com>
2023-04-30 18:56:59 +0100

    Delete dreamy/emotion_classification directory

--- dreamy/emotion_classification/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .architecture import *
-from .generation import *
-from ._test_best_model import *
-from .presence import *
-from .models_names import get_emotions_model_specifications, get_emotions_HF_names
